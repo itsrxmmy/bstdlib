451fb00968ed75dfe849d3304144219581a0b974
Rammy Aly <81774732+itsrxmmy@users.noreply.github.com>
2022-09-06 21:13:01 -0700

    Add files via upload

---
+++ string.hpp
@@ -0,0 +1,23 @@
+
+/**
+ * @file string.hpp
+ * @author formik
+ * @brief Simple version of string with additional python-like features
+ * @version 0.1
+ * @date 2022-09-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#pragma once
+#include <vector>
+#include <string>
+
+namespace bstd {
+	class string : std::string {
+		public:
+	};
+}
+
+
